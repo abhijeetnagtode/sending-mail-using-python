2fbf0785a15edb5f5bef970d1ca8ad86eeef1289
Abhijeet Nagtode <35727575+abhijeetnagtode@users.noreply.github.com>
2020-08-30 12:03:09 +0530

    Add files via upload
    this is a small python program which demonstrate how to send mail using python 
    for the e mail  login i've used environment variable because of security purpose and i also recommend to use environment variable for the credentials 
    you can also attach some images using by importing some libraries which i haven't explain in this program

---
+++ demo_mail.py
@@ -0,0 +1,19 @@
+import smtplib
+import os
+from email.message import EmailMessage
+
+''' for the crendential always use environment veriable to keep information safe'''
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+msg = EmailMessage()
+msg['Subject'] = 'date at 6 pm'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'abhijeetnagtode10@gmail.com'
+msg.set_content('lets have a coffee at evening')
+
+
+#to start the connectio
+with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+    smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+    smtp.send_message(msg)
